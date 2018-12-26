17176109a0830246cf14cb46e8d3d96aeadb34df
anishk23733 <anishk23733@gmail.com>
2018-12-26 11:26:53 -0800

    added accuracy test

--- matches.js
+++ matches.js
@@ -70,45 +70,120 @@ const saveTrainingData = async (DATASET) => {
     }
 
     json = JSON.stringify(trainingData);
-    fs.writeFile('trainingData.json', json, 'utf8', (error) => console.log);
+    fs.writeFile('./json/trainingData.json', json, 'utf8', (error) => console.log);
 
 }
 
-let processMatch = async (net, red1, red2, blue1, blue2) => {
+let processMatch = async (net, red1, red2, blue1, blue2, print = true) => {
     let blueOne = await axios(`https://api.vexdb.io/v1/get_season_rankings?season=Turning Point&team=${blue1}`);
     let blueTwo = await axios(`https://api.vexdb.io/v1/get_season_rankings?season=Turning Point&team=${blue2}`);
     let redOne = await axios(`https://api.vexdb.io/v1/get_season_rankings?season=Turning Point&team=${red1}`);
     let redTwo = await axios(`https://api.vexdb.io/v1/get_season_rankings?season=Turning Point&team=${red2}`);
-
-    console.log(net.run({
-        red1: redOne.data.result[0].vrating,
-        red2: redTwo.data.result[0].vrating,
-        blue1: blueOne.data.result[0].vrating,
-        blue2: blueTwo.data.result[0].vrating
-    }))
+    if (redOne.data.result[0] != undefined && redTwo.data.result[0] != undefined && blueOne.data.result[0] != undefined && blueTwo.data.result[0] != undefined) {
+        let result = (net.run({
+            red1: redOne.data.result[0].vrating,
+            red2: redTwo.data.result[0].vrating,
+            blue1: blueOne.data.result[0].vrating,
+            blue2: blueTwo.data.result[0].vrating
+        }));
+        if (print) {
+            console.log(result);
+        }
+        return (result);
+    } else {
+        console.log("Faulty match.");
+        return null;
+    }
 }
 
 // let applyNetwork = getNetwork(100).then((net) => {
 //     processMatch(net, "44785B", "40979G", "84710B", "44785C");
 // })
+let getModelFromTrainingData = () => {
+    const net = new brain.NeuralNetwork({
+        hiddenLayers: hiddenLayers
+    });
 
-const net = new brain.NeuralNetwork({
-    hiddenLayers: [4]
-});
+    let trainingData = [];
 
-let trainingData = [];
+    fs.readFile('./json/trainingData.json', 'utf8', async function readFileCallback(err, data) {
+        if (err) {
+            trainingData = await saveTrainingData(2000);
+            console.log("Training data saved.");
+        } else {
+            trainingData = JSON.parse(data); //now it an object
+            // trainingData = JSON.stringify(obj); //convert it back to json
+            // console.log(typeof trainingData);
+            net.train(trainingData);
 
-fs.readFile('trainingData.json', 'utf8', async function readFileCallback(err, data) {
-    if (err) {
-        trainingData = await saveTrainingData(100);
-        console.log("Training data saved.");
-    } else {
-        trainingData = JSON.parse(data); //now it an object
-        // trainingData = JSON.stringify(obj); //convert it back to json
-        // console.log(typeof trainingData);
+            let j = net.toJSON();
+            let t = JSON.stringify(j); //brain as string
+            fs.writeFile('./json/model.json', t, 'utf8', (error) => console.log);
 
-        net.train(trainingData);
+            // TEST
+            processMatch(net, "1350Z", "5327R", "824J", "5327B");
+        }
+    });
+}
 
-        processMatch(net, "40979A", "40979E", "29420B", "40979G");
+let loadModel = () => {
+    const net = new brain.NeuralNetwork();
+    const jsonFile = fs.readFileSync('./json/model.json');
+    const json = JSON.parse(jsonFile);
+    net.fromJSON(json);
+    return net;
+}
+
+let testAccuracy = async (team) => {
+    let net = loadModel();
+    let data = await axios(`https://api.vexdb.io/v1/get_matches?team=${team}&season=Turning Point`);
+    let matchCount = 0;
+    let accuracyCount = 0;
+
+    let winner = "";
+    let predictedWinner = "";
+    for (let i = 0; i < data.data.result.length; i++) {
+        // console.log("Red: " + data.data.result[i].red1 + " " + data.data.result[i].red2);
+        // console.log("Blue: " + data.data.result[i].blue1 + " " +
+        // data.data.result[i].blue2);
+        if (data.data.result[i].redscore > data.data.result[i].bluescore) {
+            // console.log("Winner: Red");
+            winner = "red";
+        } else if (data.data.result[i].redscore < data.data.result[i].bluescore) {
+            // console.log("Winner: Blue");
+            winner = "blue";
+        } else {
+            // console.log("Tie");
+            winner = "none";
+        }
+
+        let predictions = await processMatch(net, data.data.result[i].red1, data.data.result[i].red2, data.data.result[i].blue1, data.data.result[i].blue2, false);
+
+        if (predictions != null) {
+            matchCount++;
+            if (predictions["red"] > predictions["blue"]) {
+                predictedWinner = "red";
+            } else if (predictions["red"] < predictions["blue"]) {
+                predictedWinner = "blue";
+            } else {
+                predictedWinner = "none";
+            }
+
+            if (winner == predictedWinner) {
+                accuracyCount++;
+            }
+
+            // console.log(`${accuracyCount} / ${matchCount} correct.`)
+        } else {
+            console.log("Match not recorded.");
+        }
     }
-});
+
+    console.log("Overall accuracy: " + ((accuracyCount / matchCount) * 100) + "%")
+}
+
+// let teams = ["5327B", "5327R", "5327C", "5327X", "5327S"];
+
+// for (let i in teams) {
+//     testAccuracy(teams[i]).then(() => console.log(teams[i]));
+// }
